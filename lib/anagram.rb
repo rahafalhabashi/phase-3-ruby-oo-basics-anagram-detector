e87af54a24615ab73d53fe344e2c5a3183a41d91
Rahaf Alhabashi <rahafalhabashi@gmail.com>
2022-04-28 12:02:04 -0400

    commit tree f96b8af5a046788c1d594171b7e94f23f44c4da0

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
-# Your code goes here!
+class Anagram
+    def initialize(word)
+        @word = word
+    end
+
+    def match 
+        possible_words = []
+        if word != possible_words
+
+        end
+    end
+end
